152401a1cfa8af93edde52141767a4b22e71b058
sriram-ac <sriram@atomcare.in>
2023-12-05 13:21:25 +0530

    setup security firbase

--- firebase-get-to-know-flutter/step_02/lib/app_state.dart
+++ firebase-get-to-know-flutter/step_02/lib/app_state.dart
@@ -7,13 +7,30 @@ import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
-import 'guest_book_message.dart';                        
+import 'guest_book_message.dart';       
+enum Attending { yes, no, unknown }                 
 
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
     init();
   }
 
+int _attendees = 0;
+int get attendees => _attendees;
+
+Attending _attending = Attending.unknown;
+StreamSubscription<DocumentSnapshot>? _attendingSubscription;
+Attending get attending => _attending;
+set attending(Attending attending) {
+  final userDoc = FirebaseFirestore.instance
+      .collection('attendees')
+      .doc(FirebaseAuth.instance.currentUser!.uid);
+  if (attending == Attending.yes) {
+    userDoc.set(<String, dynamic>{'attending': true});
+  } else {
+    userDoc.set(<String, dynamic>{'attending': false});
+  }
+}
   bool _loggedIn = false;
   bool get loggedIn => _loggedIn;
 
@@ -29,6 +46,14 @@ class ApplicationState extends ChangeNotifier {
       EmailAuthProvider(),
     ]);
 
+        FirebaseFirestore.instance
+        .collection('attendees')
+        .where('attending', isEqualTo: true)
+        .snapshots()
+        .listen((snapshot) {
+      _attendees = snapshot.docs.length;
+      notifyListeners();
+    });
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
         _loggedIn = true;
@@ -49,10 +74,28 @@ class ApplicationState extends ChangeNotifier {
           }
           notifyListeners();
         });
+        _attendingSubscription = FirebaseFirestore.instance
+            .collection('attendees')
+            .doc(user.uid)
+            .snapshots()
+            .listen((snapshot) {
+          if (snapshot.data() != null) {
+            if (snapshot.data()!['attending'] as bool) {
+              _attending = Attending.yes;
+            } else {
+              _attending = Attending.no;
+            }
+          } else {
+            _attending = Attending.unknown;
+          }
+          notifyListeners();
+        });
       } else {
         _loggedIn = false;
+       // _emailVerified = false;
         _guestBookMessages = [];
         _guestBookSubscription?.cancel();
+         _attendingSubscription?.cancel(); // new
       }
       notifyListeners();
     });
