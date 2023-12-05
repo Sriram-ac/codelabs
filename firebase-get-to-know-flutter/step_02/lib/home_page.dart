c0c37e9e8cb53112b51b7b5536a917dfdb9390c7
sriram-ac <sriram@atomcare.in>
2023-12-05 11:50:00 +0530

    write message to firebase

--- firebase-get-to-know-flutter/step_02/lib/home_page.dart
+++ firebase-get-to-know-flutter/step_02/lib/home_page.dart
@@ -1,14 +1,18 @@
 // Copyright 2022 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-       // new
+// new
 import 'package:firebase_auth/firebase_auth.dart' // new
-    hide EmailAuthProvider, PhoneAuthProvider;    // new
-import 'package:flutter/material.dart';           // new
-import 'package:provider/provider.dart';          // new
+    hide
+        EmailAuthProvider,
+        PhoneAuthProvider; // new
+import 'package:flutter/material.dart'; // new
+import 'package:gtk_flutter/guest_book.dart';
+import 'package:provider/provider.dart'; // new
 
-import 'app_state.dart';                          // new
-import 'src/authentication.dart';                 // new
+import 'app_state.dart'; // new
+import 'package:gtk_flutter/guest_book.dart';                         // new
+import 'src/authentication.dart'; // new
 import 'src/widgets.dart';
 
 class HomePage extends StatelessWidget {
@@ -27,7 +31,7 @@ class HomePage extends StatelessWidget {
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
 
-           Consumer<ApplicationState>(
+          Consumer<ApplicationState>(
             builder: (context, appState, _) => AuthFunc(
                 loggedIn: appState.loggedIn,
                 signOut: () {
@@ -44,6 +48,20 @@ class HomePage extends StatelessWidget {
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+           Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),
